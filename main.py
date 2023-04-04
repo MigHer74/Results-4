f58d8302dde9071d90df01277e90e17891db83a9
Miguel Hernandez <migher74@outlook.com>
2023-04-04 16:02:38 -0600

    Add main menu partial funcionality

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import os
 
 os.system("cls")
 
-mainMenu = "Main Menu\n\nS - Short Game\nL - Long Game\nR - Replay Game\n\nX - Exit"
+mainMenu = "Main Menu\n\nP - Play Game\nR - Replay Game\nE - Enter Previous Data\nV - View Data\nD - Delete Data\n-----------------------\nX - Exit"
 
 print(mainMenu)
 mainSelect = input("Please, choose an option: ")
+
+match mainSelect:
+    case "P" | "p" : print("You choose play a game.")
+    case "R" | "r" : print("You choose replay a game.")
+    case "E" | "e" : print("You choose enter previous data.")
+    case "V" | "v" : print("You choose view data of the database.")
+    case "D" | "d" : print("You choose delete data of the database.")
+    case "X" | "x" : print("You choose exit the game.")
+    case _: input("\nInvalid selection, please try again. (Press any key to continue)")
