dd2c8d728dc3a1778b50f21b0010fd3786b235fd
Miguel Hernandez <migher74@outlook.com>
2023-04-04 15:32:16 -0600

    Create main program.

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("cls")
+
+mainMenu = "Main Menu\n\nS - Short Game\nL - Long Game\nR - Replay Game\n\nX - Exit"
+
+print(mainMenu)
+mainSelect = input("Please, choose an option: ")
