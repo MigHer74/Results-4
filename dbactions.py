4f5dea796f575048d4e30d89417d830081d7ef09
Miguel Hernandez <migher74@outlook.com>
2023-04-19 19:58:31 -0600

    Add create table function.

--- dbactions.py
+++ dbactions.py
@@ -3,3 +3,12 @@ import sqlite3
 def connect_db():
     connection_db = sqlite3.connect("results.db")
     return connection_db
+
+def create_tbl():
+    cnx = connect_db()
+    cur = cnx.cursor()
+    scpFile = open("table.sql")
+    scpVar = scpFile.read()
+    cur.executescript(scpVar)
+    cnx.commit()
+    cnx.close()
