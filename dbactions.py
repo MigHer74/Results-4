9c2996f5cf16b47cc2ed7e5c8c77f919c92da5dc
Miguel Hernandez <migher74@outlook.com>
2023-04-21 20:48:33 -0600

    Add the function to create the data table.

--- dbactions.py
+++ dbactions.py
@@ -4,11 +4,32 @@ def connect_db():
     connection_db = sqlite3.connect("results.db")
     return connection_db
 
-def create_tbl():
+def verify_tbl():
     cnx = connect_db()
     cur = cnx.cursor()
-    scpFile = open("table.sql")
-    scpVar = scpFile.read()
-    cur.executescript(scpVar)
+    cur.execute("""CREATE TABLE IF NOT EXISTS "games" (
+	gtype	VARCHAR(1),
+	gweek	VARCHAR(2),
+	ggame	INT,
+	n1	INT,
+	n2	INT,
+	n3	INT,
+	n4	INT,
+	n5	INT,
+	n6	INT)""")
     cnx.commit()
     cnx.close()
+
+def check_data():
+    verify = True
+    while verify:
+        try:
+            cnx = connect_db()
+            cur = cnx.cursor()
+            cur.execute("SELECT * FROM games")
+            curData = cur.fetchall()
+            verify = False
+        except sqlite3.OperationalError:
+            create_tbl()
+    
+    return curData
